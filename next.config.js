f4f33c6c268ba84dd5c1bb4cb0096bc5bdaa98de
Anvesh <anvesh.poda@justdial.com>
2023-05-19 17:00:29 +0530

    Port changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  env:{
+    collectionId: process.env.collectionId,
+    apiKey: process.env.apiKey
+  }
 }
 
 module.exports = nextConfig
