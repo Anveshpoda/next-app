78895feaddba04006c02eadc8fb088da82c8f098
Anvesh <podaanvesh@gmail.com>
2024-06-10 19:15:16 +0530

    Added Pipeline

--- pages/el_new.js
+++ pages/el_new.js
@@ -39,10 +39,11 @@ export default function El_new() {
     return (
         <div style={{ margin: 10 }}>
             <h1>EL Staging Script</h1>
-            <Button variant="contained" color="secondary" onClick={runScript} disabled={loading} endIcon={loading && <CircularProgress size={24} />}
-                style={{ backgroundColor: color ? color : 'secondary', color: 'white' }}
-            > {loading ? 'Loading...' : 'Submit'}
+            <Button variant="contained" color="primary" onClick={runScript} disabled={loading} endIcon={loading && <CircularProgress size={24} />}
+                style={{ backgroundColor: color ? color : 'primary', color: 'white' }}>
+                {loading ? 'Loading...' : 'Submit'}
             </Button>
+            {color == "green" && <Button variant="contained" color="secondary" style={{ marginLeft: 20 }} onClick={() => { window.location.href = "http://192.168.131.150:8980/job/git_update_edit-list_staging/" }}> Run Pipeline </Button>}
             {(output || error) && <div className="tranBg" style={{ maxHeight: 'calc(100vh - 154px)', overflow: 'scroll', width: '100%', marginTop: 10 }}>
                 {output && <pre>{output}</pre>}
                 {error && <pre style={{ color: 'red' }}>{error}</pre>}
