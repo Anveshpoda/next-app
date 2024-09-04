8ab7c55f7640eeda0ab15a593dea0b94fdd66bde
Anvesh <podaanvesh@gmail.com>
2024-09-04 18:19:15 +0530

    echo added

--- el_hotfix.sh
+++ el_hotfix.sh
@@ -63,7 +63,9 @@ echo "_________________________ COPY LIVE BUILD TO project01.anveshpoda.blrsoftw
 rsync -ah -J --exclude='node_modules' --exclude='package.json' --exclude='src' --exclude='public' ./edit_list/ ../project01/MSITE/edit_list
 rsync -ah -J --exclude='node_modules' --exclude='package.json' --exclude='src' ./edit_list/ ../project01/edit_list
 
-echo "Copied Successfully"
+echo " Build Copied Successfully. Check your changes in this PORT"
+echo " http://project01.anveshpoda.blrsoftware.jd/MSITE/edit_list/index.php"
+echo " "
 
 echo "_________________________ GETTING UNIQUE JIRA IDs WITH URLS FROM el-hotfix ______________________________"
 
