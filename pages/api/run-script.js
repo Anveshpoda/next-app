544df8a76e7ad8fa0561d40b52b0d8443ccadf3a
Anvesh <podaanvesh@gmail.com>
2024-09-04 16:45:48 +0530

    log

--- pages/api/run-script.js
+++ pages/api/run-script.js
@@ -33,7 +33,7 @@ export default function handler(req, res) {
         // const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Generate timestamp for file naming
         // const logFileName = `${LOG_DIR}${type}/log-${timestamp}.txt`;
         // const logFileName = `log_${new Date().toLocaleString('en-GB', { hour: '2-digit', minute: '2-digit', day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/[/,]/g, '-').replace(' ', '__').replace(':', '-')}.txt`;
-        const logFileName = `log_${moment().format('HH-mm__DD-MM-YYYY')}.txt`;
+        const logFileName = `${LOG_DIR}${type}/log_${moment().format('HH-mm__DD-MM-YYYY')}.txt`;
         console.log('logFileName >> ',logFileName)
         const scriptPath = path.resolve(type == 'dev' ? 'el_dev.sh' : type == 'hotfix' ? 'el_hotfix.sh' : '/home/anveshpoda/el_beta.sh');
         
