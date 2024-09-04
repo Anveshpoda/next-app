9f29ba16165b665e38d2ea90666c6f0449511ca9
Anvesh <podaanvesh@gmail.com>
2024-09-04 16:21:06 +0530

    change file names

--- pages/api/run-script.js
+++ pages/api/run-script.js
@@ -29,8 +29,9 @@ export default function handler(req, res) {
             });
         }
 
-        const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Generate timestamp for file naming
-        const logFileName = `${LOG_DIR}${type}/log-${timestamp}.txt`;
+        // const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Generate timestamp for file naming
+        // const logFileName = `${LOG_DIR}${type}/log-${timestamp}.txt`;
+        const logFileName = `log_${new Date().toLocaleString('en-GB', { hour: '2-digit', minute: '2-digit', day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/[/,]/g, '-').replace(' ', '__').replace(':', '-')}.txt`;
         const scriptPath = path.resolve(type == 'dev' ? 'el_dev.sh' : type == 'hotfix' ? 'el_hotfix.sh' : '/home/anveshpoda/el_beta.sh');
         
         // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
