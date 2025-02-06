828dde1126c20152024d507742262b4ffe087bee
Anvesh <podaanvesh@gmail.com>
2025-02-06 16:33:52 +0530

    wehook check

--- pages/api/test/webhook.js
+++ pages/api/test/webhook.js
@@ -1,6 +1,7 @@
 import { slackLog } from "@/utils/fun";
 
 export default async function handler(req, res) {
-  slackLog('Webhook received successfully!');
+    const { query , body } = req
+  slackLog(JSON.stringify({ Msg: 'Webhook received successfully!', body, query}));
   res.status(200).json({state: 'success', message: 'Webhook received successfully!'});
 }
