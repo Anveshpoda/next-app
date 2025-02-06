19a616c6d5073d33cef94b356082b83984a0a271
Anvesh <podaanvesh@gmail.com>
2025-02-06 20:43:51 +0530

    webhook

--- pages/api/el/webhook.js
+++ pages/api/el/webhook.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
         return res.status(200).json({ message: 'Webhook handled successfully' });
     } catch (error) {
         console.error('Error handling webhook:', error);
+        slackLog(JSON.stringify({ Msg: 'Error processing the webhook', error: error}));
         return res.status(500).json({ message: 'Error processing the webhook', error: error });
     }
     // res.status(200).json({ state: 'success', message: 'Webhook received successfully!' });
